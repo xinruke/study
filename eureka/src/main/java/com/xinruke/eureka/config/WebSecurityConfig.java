e27a6cb8956b1cece25715c77f34b2090809dabd
fengxinqiang <fxq230@163.com>
2019-05-30 16:33:49 +0800

    Eureka增加Security

--- eureka/src/main/java/com/xinruke/eureka/config/WebSecurityConfig.java
+++ eureka/src/main/java/com/xinruke/eureka/config/WebSecurityConfig.java
@@ -10,5 +10,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        super.configure(http);
     }
 }
