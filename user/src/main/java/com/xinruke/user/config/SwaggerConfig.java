a29a8fc05f921d49a33e801810b8982aee9bc837
fengxinqiang <fxq230@163.com>
2019-06-03 16:08:02 +0800

    增加Swagger、Task、Filter、Interceptor

--- user/src/main/java/com/xinruke/user/config/SwaggerConfig.java
+++ user/src/main/java/com/xinruke/user/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.xinruke.user.config;
 import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @Configuration 让Spring加载该类配置
  * @EnableSwagger2 启用Swagger2
  */
+@Profile({"dev", "test"})
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
