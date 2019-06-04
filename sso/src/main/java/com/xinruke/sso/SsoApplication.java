5dd54f350ef38c042eccba59bacce75822fbba3b
fengxinqiang <fxq230@163.com>
2019-06-04 09:18:38 +0800

    增加 SpringBootAdmin

--- sso/src/main/java/com/xinruke/sso/SsoApplication.java
+++ sso/src/main/java/com/xinruke/sso/SsoApplication.java
@@ -1,13 +1,19 @@
 package com.xinruke.sso;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SsoApplication {
+    static Logger logger = LoggerFactory.getLogger(SsoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SsoApplication.class, args);
-    }
 
+        logger.info("--------------------------");
+        logger.info("----Sso Start Success----");
+        logger.info("--------------------------");
+    }
 }
