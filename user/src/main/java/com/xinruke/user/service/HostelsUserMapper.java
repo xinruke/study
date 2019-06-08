78567126500824a6fb54467414b40d094b7bcba0
GuoZheng <guozheng@miaoyouche.com>
2019-06-08 17:25:40 +0800

    1.maven-继承 2.support中generator类

---
+++ user/src/main/java/com/xinruke/user/service/HostelsUserMapper.java
@@ -0,0 +1,7 @@
+package com.xinruke.user.service;
+
+import com.xinruke.user.po.HostelsUser;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface HostelsUserMapper extends Mapper<HostelsUser> {
+}
